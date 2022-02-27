17ca7e4b6a5653f752d2e76cc0cf197565a0a327
ardama <damaarunkumar9@gmail.com>
2022-02-27 20:13:21 +0530

    terraform

--- 01.ec2-sg/sg.tf
+++ 01.ec2-sg/sg.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow_tls" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   }
