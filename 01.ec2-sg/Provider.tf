38dc7b7163e41fb76d8fc844807d15143f843ffb
ardama <damaarunkumar9@gmail.com>
2022-02-27 20:49:18 +0530

    terraform

--- 01.ec2-sg/Provider.tf
+++ 01.ec2-sg/Provider.tf
@@ -1,3 +1,11 @@
-provider "aws"   {
+provider "aws" {
   region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-b62"
+    key    = "sample/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
